7a5511ee74393b19ecda9d378f69ff246fc64c76
ayooon <michaunapierala@gmail.com>
2024-03-19 19:04:38 +0100

    Add a function to determine the winner

---
+++ script.js
@@ -0,0 +1,23 @@
+currentScore = ['x', 'x', 'o', 'x', 'o', 'x', 'x', 'x', 'x']
+function checkWinner(cs) {
+    ;
+    const combs = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    combs.forEach(el => {
+        if (
+            cs[el[0]] == cs[el[1]] && cs[el[1]] == cs[el[2]] && cs[el[0]]!=''
+        ){
+              console.log (cs[el[0]]+' wins')
+        }
+    
+});
+}
+console.log(checkForWinner(currentScore))
